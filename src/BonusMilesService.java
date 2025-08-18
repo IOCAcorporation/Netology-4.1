5d8617b1eef54d280056c70d9b2ccdbafbb855e8
IOCAcorporation <hri.st@yandex.com>
2025-08-18 19:03:57 +0300

    Delete src directory

--- src/BonusMilesService.java
+++
@@ -1,6 +0,0 @@
-public class BonusMilesService {
-    public int calculate(int miles) {
-        int answer = 10000 / miles;
-        return answer;
-    }
-}
--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BonusMilesService service = new BonusMilesService();
-        int answer = service.calculate(20); // должно получиться 500
-        System.out.println(answer + " - Счет милей.");
-        }
-    }
-
